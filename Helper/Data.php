4c2b4e46c383a962fd8198c0caf0877ec2d6a748
Fabio Brunelli <arioch1984@gmail.com>
2018-07-06 14:11:33 +0200

    bugfix: added only CMS block use

--- Helper/Data.php
+++ Helper/Data.php
@@ -7,6 +7,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper{
     const POPUP_ENABLED = 'newsletter/popup/enabled';
     const ONLY_CMS = 'newsletter/popup/only_cms';
 
+    const CMS_BLOCK_ID = 'newsletter.popup.upper.content';
+
     public function getPopupEnabled()
     {
         return $this->scopeConfig->getValue(
