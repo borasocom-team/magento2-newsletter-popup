05b8ac2d5ed6bdc5433e5d72770f805aafb2ba01
Fabio Brunelli <arioch1984@gmail.com>
2018-07-06 11:02:56 +0200

    added only CMS block use

--- Block/Subscribe.php
+++ Block/Subscribe.php
@@ -2,9 +2,9 @@
 
 /**
  * @Author: Ngo Quang Cuong
- * @Date:   2017-10-29 21:58:28
- * @Last Modified by:   https://www.facebook.com/giaphugroupcom
- * @Last Modified time: 2017-10-29 22:26:47
+ * @Date  :   2017-10-29 21:58:28
+ * @Last  Modified by:   https://www.facebook.com/giaphugroupcom
+ * @Last  Modified time: 2017-10-29 22:26:47
  */
 
 /**
@@ -19,36 +19,40 @@ use Magento\Customer\Model\Session;
 use Magento\Framework\Stdlib\CookieManagerInterface;
 use Magento\Framework\View\Element\Template;
 use Magento\Newsletter\Model\Subscriber;
+use PHPCuong\Newsletter\Helper;
 
 class Subscribe extends \Magento\Framework\View\Element\Template
 {
+
     const NEWSLETTER_POPUP_COOKIE_NAME = 'newsletter_popup';
 
     protected $customerSession;
     protected $newsletterSubscriber;
     protected $cookieManager;
+    protected $helper;
 
     public function __construct(
         Template\Context $context,
         Session $session,
         Subscriber $subscriber,
         CookieManagerInterface $cookie,
+        Data $helper,
         array $data = []
-    )
-    {
-        $this->customerSession = $session;
+    ) {
+        $this->customerSession      = $session;
         $this->newsletterSubscriber = $subscriber;
-        $this->cookieManager = $cookie;
+        $this->cookieManager        = $cookie;
+        $this->helper               = $helper;
 
-        $customerId = $this->customerSession->getCustomerId();
+        $customerId                = $this->customerSession->getCustomerId();
         $checkNewsletterSubscriber = false;
 
-        if(is_numeric($customerId)){
+        if (is_numeric($customerId)) {
             $checkNewsletterSubscriber = $this->newsletterSubscriber->loadByCustomerId($customerId);
         }
 
-        if($checkNewsletterSubscriber && $checkNewsletterSubscriber->isSubscribed()){
-            $this->cookieManager->setPublicCookie(self::NEWSLETTER_POPUP_COOKIE_NAME,1);
+        if ($checkNewsletterSubscriber && $checkNewsletterSubscriber->isSubscribed()) {
+            $this->cookieManager->setPublicCookie(self::NEWSLETTER_POPUP_COOKIE_NAME, 1);
         }
 
         parent::__construct($context, $data);
@@ -63,5 +67,14 @@ class Subscribe extends \Magento\Framework\View\Element\Template
     public function getFormActionUrl()
     {
         return $this->getUrl('phpcuong_newsletter/subscriber/new', ['_secure' => true]);
+    }
+
+    public function isEnabled()
+    {
+        return $this->helper->getPopupEnabled();
+    }
+
+    public function onlyCms(){
+        return $this->helper->getOnlyCms();
     }
 }
---
+++ Helper/Data.php
@@ -0,0 +1,23 @@
+<?php
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper{
+
+    const POPUP_ENABLED = 'newsletter/popup/enabled';
+    const ONLY_CMS = 'newsletter/popup/only_cms';
+
+    public function getPopupEnabled()
+    {
+        return $this->scopeConfig->getValue(
+            self::POPUP_ENABLED,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+    
+    public function getOnlyCms()
+    {
+        return $this->scopeConfig->getValue(
+            self::ONLY_CMS,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+    }
+}
